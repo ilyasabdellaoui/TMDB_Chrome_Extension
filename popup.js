fdf80386c4fd1eeabe6aebc9b52f67f3b302ede9
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2023-07-08 17:38:06 +0100

    Reload webpage after setting api key in popup

--- popup.js
+++ popup.js
@@ -1,23 +1,26 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const apiKeyInput = document.getElementById('apiKeyInput');
-    const submitButton = document.getElementById('submitButton');
-  
-    submitButton.addEventListener('click', function () {
-      const apiKey = apiKeyInput.value.trim();
-  
-      if (apiKey) {
-        // Store the API key in Chrome storage
-        chrome.runtime.sendMessage({ type: 'setApiKey', apiKey: apiKey }, (response) => {
-          if (response) {
-            console.log('API key:', apiKey);
+  const apiKeyInput = document.getElementById('apiKeyInput');
+  const submitButton = document.getElementById('submitButton');
+
+  submitButton.addEventListener('click', function () {
+    const apiKey = apiKeyInput.value.trim();
+
+    if (apiKey) {
+      // Store the API key in Chrome storage
+      chrome.runtime.sendMessage({ type: 'setApiKey', apiKey: apiKey }, (response) => {
+        if (response) {
+          console.log('API key:', apiKey);
+          chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            const activeTab = tabs[0];
+            chrome.tabs.reload(activeTab.id);
             window.close(); // Close the popup after storing the API key
-          } else {
-            console.error('Error storing API key');
-          }
-        });
-      } else {
-        console.log('No API key provided');
-      }
-    });
+          });
+        } else {
+          console.error('Error storing API key');
+        }
+      });
+    } else {
+      console.log('No API key provided');
+    }
   });
-  
+});
