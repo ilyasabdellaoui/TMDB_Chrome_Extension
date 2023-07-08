613c1afe633932378c5ec06ba9cd3ec6c11fd639
ilyasabdellaoui <ilyas.abdellaoui21@gmail.com>
2023-07-08 20:32:45 +0100

    Add To watchlist feature

--- popup.js
+++ popup.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
   const apiKeyInput = document.getElementById('apiKeyInput');
   const submitButton = document.getElementById('submitButton');
+  const addWatchListButton = document.getElementById('addWatchList');
+  const watchListInput = document.getElementById('watchListInput');
+  const submitWatchListButton = document.getElementById('submitWatchListButton');
+  const initialContent = document.getElementById('initialContent');
+  const watchListContent = document.getElementById('watchListContent');
 
   submitButton.addEventListener('click', function () {
     const apiKey = apiKeyInput.value.trim();
@@ -21,6 +26,21 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     } else {
       console.log('No API key provided');
+    }
+  });
+
+  addWatchListButton.addEventListener('click', function () {
+    initialContent.style.display = 'none';
+    watchListContent.style.display = 'block';
+  });
+
+  submitWatchListButton.addEventListener('click', function () {
+    const movieList = watchListInput.value.trim();
+    if (movieList) {
+      // Process the movie list and add it to the watchlist
+      // ...
+    } else {
+      console.log('No movie list provided');
     }
   });
 });
